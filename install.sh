05cdd8cb12e02c55b86551996d8671aacb8f9754
John Stonty <1844063767@qq.com>
2022-04-18 04:23:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ pip install pyotp pyqrcode
 systemctl start rc-local
 systemctl enable rc-local
 mkdir /etc/otp
-wget -O /etc/otp/otp.py https://raw.githubusercontent.com/sht2017/otp/main/otp.py
+wget --no-cache -O /etc/otp/otp.py https://raw.githubusercontent.com/sht2017/otp/main/otp.py
 chmod +x /etc/otp/otp.py
-wget -O /etc/otp/setup.py https://raw.githubusercontent.com/sht2017/otp/main/setup.py
+wget --no-cache -O /etc/otp/setup.py https://raw.githubusercontent.com/sht2017/otp/main/setup.py
 chmod +x /etc/otp/setup.py
 echo "screen -dmS otp-service /etc/otp/otp.py" >> /etc/rc.local
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
